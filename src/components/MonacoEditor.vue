93370052daddaad4bb91c9a7116cc885820ede3c
xueming <810505339@qq.com>
2022-07-25 00:55:16 +0800

    添加monaco

---
+++ src/components/MonacoEditor.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import * as monaco from 'monaco-editor'
+const codeBox = $ref<HTMLHtmlElement | null>(null)
+const option: monaco.editor.IStandaloneEditorConstructionOptions = {
+  value: '',
+  language: 'json',
+  theme: 'vs-dark',
+  automaticLayout: false,
+  minimap: {
+    enabled: false,
+  },
+}
+
+const init = () => {
+  nextTick(() => {
+    monaco.editor.create(codeBox!, option)
+    console.log(1)
+  })
+}
+init()
+</script>
+
+<template>
+  <div ref="codeBox" h-xl />
+</template>
